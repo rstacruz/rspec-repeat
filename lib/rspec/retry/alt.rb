3135e69236d8b1cf8de6d9b9b49eea472d7d46bd
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-09-24 23:25:07 +0800

    Make it work

--- lib/rspec/retry/alt.rb
+++ lib/rspec/retry/alt.rb
@@ -13,19 +13,30 @@ module RSpec
       #     end
       #
       def rerun(ex, count, options = {})
+        example = RSpec.current_example
         wait = options[:wait]
         exceptions = options[:exceptions]
+        verbose = options[:verbose]
+        verbose = !!ENV['RSPEC_RETRY_VERBOSE'] if verbose.nil?
         clear_lets = options[:clear_lets]
         clear_lets = true if clear_lets.nil?
 
         count.times do |i|
-          example.clear_exception
-          example.run
+          example.instance_variable_set :@exception, nil
+          ex.run
           break if example.exception.nil?
-          break if Utils.matches_exceptions?(exceptions, example.exception)
-          Utils.print_failure(i, example) if i < count - 1
+          break if 
+          Utils.print_failure(i, example) if verbose
           self.clear_lets if clear_lets
           sleep wait if wait.to_i > 0
+        end
+      end
+
+      def clear_lets
+        if respond_to? :__init_memoized, true
+          __init_memoized
+        else
+          @__memoized = nil
         end
       end
 
@@ -34,13 +45,25 @@ module RSpec
 
         def print_failure(i, example)
           msg =
-            "RSpecRetryAlt: #{i + 1} try error in #{example.location}:\n" \
+            "RSpecRetryAlt: #{nth(i + 1)} try error in #{example.location}:\n" \
             "  #{example.exception}\n"
-          RSpec.configuration.reporter.message(msg)
+            puts msg
+        end
+
+        def nth(n)
+          if n % 10 == 1 && n != 11
+            "#{n}st"
+          elsif n % 10 == 2 && n != 11
+            "#{n}nd"
+          elsif n % 10 == 3 && n != 11
+            "#{n}rd"
+          else
+            "#{n}th"
+          end
         end
 
         def matches_exceptions?(exceptions, exception)
-          return unless exceptions.any?
+          return unless exceptions
           exceptions.any? do |exception_klass|
             exception.is_a?(exception_klass)
           end
--- spec/retry_spec.rb
+++ spec/retry_spec.rb
@@ -4,10 +4,10 @@ describe 'retry' do
   include RSpec::Retry::Alt
 
   around do |example|
-    rerun example, 3
+    rerun example, 20
   end
 
   it 'works' do
-    expect(rand(2)).to eq 0
+    expect(rand(10)).to eq 0
   end
 end
